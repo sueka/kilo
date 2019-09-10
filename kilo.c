1305d8e1f4badb2fa4c50ea9da540846936c5adf
sueka <mail@sueka.me>
2019-09-11 05:15:29 +0900

    :sparkles: Enable you to move cursor using WASD key, w/ an overflow bug

--- kilo.c
+++ kilo.c
@@ -166,6 +166,23 @@ void editorRefreshScreen() {
 
 /*** input ***/
 
+void editorMoveCursor(char key) {
+  switch (key) {
+    case 'a':
+      E.cx--;
+      break;
+    case 'd':
+      E.cx++;
+      break;
+    case 'w':
+      E.cy--;
+      break;
+    case 's':
+      E.cy++;
+      break;
+  }
+}
+
 void editorProcessKeypress() {
   char c = editorReadKey();
 
@@ -174,6 +191,13 @@ void editorProcessKeypress() {
       write(STDOUT_FILENO, "\x1b[2J", 4);
       write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
+      break;
+
+    case 'w':
+    case 'a':
+    case 's':
+    case 'd':
+      editorMoveCursor(c);
       break;
   }
 }
