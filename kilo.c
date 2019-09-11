6eea1bc6c1fcac7651ae890a921b37532f917f03
sueka <mail@sueka.me>
2019-09-11 15:39:50 +0900

    :sparkles: Recognize delete key

--- kilo.c
+++ kilo.c
@@ -20,6 +20,7 @@ enum editorKey {
   ARROW_RIGHT,
   ARROW_UP,
   ARROW_DOWN,
+  DEL_KEY, // TODO: rename?
   HOME_KEY, // TODO: rename?
   END_KEY, // TODO: rename?
   PAGE_UP,
@@ -85,6 +86,7 @@ int editorReadKey() {
         if (seq[2] == '~') {
           switch (seq[1]) {
             case '1': return HOME_KEY;
+            case '3': return DEL_KEY;
             case '4': return END_KEY;
             case '5': return PAGE_UP;
             case '6': return PAGE_DOWN;
