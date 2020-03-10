f07e827ebaed0030cf7311edefe0c1ddfc67732e
sueka <mail@sueka.me>
2020-03-10 22:26:58 +0900

    :rotating_light: Stop cursor at the end of line

--- kilo.c
+++ kilo.c
@@ -286,6 +286,8 @@ void editorRefreshScreen() {
 /*** input ***/
 
 void editorMoveCursor(int key) {
+  erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+
   switch (key) {
     case ARROW_LEFT:
       if (E.cx > 0) {
@@ -293,9 +295,9 @@ void editorMoveCursor(int key) {
       }
       break;
     case ARROW_RIGHT:
-      // if (E.cx < E.numcols) { // E.numcols: the number of charcters of the longest row
+      if (row && E.cx < row->size) {
         E.cx++;
-      // }
+      }
       break;
     case ARROW_UP:
       if (E.cy > 0) {
