f0468ecc0f8fc8d5471cccc638e47526886c70e7
sueka <mail@sueka.me>
2020-03-10 18:24:35 +0900

    :sparkles: Update coloff as cx updates

--- kilo.c
+++ kilo.c
@@ -215,12 +215,18 @@ void abFree(struct abuf *ab) {
 
 /*** output ***/
 
-void editorScroll() {
+void editorScroll() { // TODO: delete ifs
   if (E.cy < E.rowoff) {
     E.rowoff = E.cy;
   }
   if (E.cy >= E.rowoff + E.screenrows) {
     E.rowoff = E.cy - E.screenrows + 1;
+  }
+  if (E.cx < E.coloff) {
+    E.coloff = E.cx;
+  }
+  if (E.cx >= E.coloff + E.screencols) {
+    E.coloff = E.cx - E.screencols + 1;
   }
 }
 
