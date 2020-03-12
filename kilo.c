8abf4a4254b91a1f1836aadbd18cebcaea56d975
sueka <mail@sueka.me>
2020-03-12 14:28:03 +0900

    :rotating_light: Get proper rx

--- kilo.c
+++ kilo.c
@@ -261,7 +261,10 @@ void abFree(struct abuf *ab) {
 /*** output ***/
 
 void editorScroll() { // TODO: delete ifs
-  E.rx = E.cx;
+  E.rx = 0;
+  if (E.cy < E.numrows) {
+    E.rx = editorRowCxToRx(&E.row[E.cy], E.cx);
+  }
 
   if (E.cy < E.rowoff) {
     E.rowoff = E.cy;
